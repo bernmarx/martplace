a29cc900cc77c73e959fcef49c9e62ff714f594c
bernmarx <d.strelkin@inbox.ru>
2021-03-01 14:55:17 +0700

    started weekly products

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $(".showcase_info-rating").rateYo({
+        rating: 4.5,
+        starWidth: "12px",
+        readOnly: true,
+        spacing: "3px",
+        ratedFill: "#ffc000",
+        normalFill: "#ffffff"
+    });
+
+});
