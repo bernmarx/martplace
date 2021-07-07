d40cd12187f92c6bc059b97858848b19488ace35
bernmarx <d.strelkin@inbox.ru>
2021-07-07 17:38:46 +0700

    finished making slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,19 @@
-$(function(){
+$(function () {
 
     $(".showcase_info-rating").rateYo({
-        rating: 4.5,
+        rating: 5,
         starWidth: "12px",
         readOnly: true,
         spacing: "3px",
         ratedFill: "#ffc000",
         normalFill: "#ffffff"
+    });
+
+    $('.weekly_products-slider').slick({
+        infinite: true,
+        appendArrows: '.weekly_products_top',
+        nextArrow: '<button type="button" class="slick-next icon-chevron-right" style=""></button>',
+        prevArrow: '<button type="button" class="slick-prev icon-chevron-left" style=""></button>',
     });
 
 });
